4530e7ad09a75c5feda4c382532e0762c470e1fc
Misu <2ez4misu@gmail.com>
2022-04-29 21:41:55 +0900

    do1

--- part_1/str.py
+++ part_1/str.py
@@ -1,7 +1,4 @@
 #các kiến thức về chuỗi:
-from tkinter import N
-
-
 a = "I'm Misu"
 #nói chung chuỗi đơn giản là đoạn ký tự nằm trong cặp nháy đơn '' hoặc nháy kép "" thôi.
 
@@ -14,3 +11,11 @@ a = "I'm Misu"
 "\'" #in ra ký tự '
 "\"" #in ra ký tự "
 "\\" #in ra ký tự \
+
+#củng cố:
+# 'nasdfiuqwnerp', #đúng
+# "234a'adadf", #đúng
+# """asd34'asdfjoaisdfadf""", #đúng
+# "\", #sai
+# """\""", #sai
+# '' #đúng
