55e48839488ae94cbbf13b3ebc5e79eb693bc612
Misu <2ez4misu@gmail.com>
2022-04-29 21:23:54 +0900

    str

--- part_1/str.py
+++ part_1/str.py
@@ -1,1 +1,7 @@
-#các kiến thức về chuỗi
+#các kiến thức về chuỗi:
+a = "I'm Misu"
+#nói chung chuỗi đơn giản là đoạn ký tự nằm trong cặp nháy đơn '' hoặc nháy kép "" thôi.
+
+#Escape Sequence \
+#Có nhiều kiểu sử dụng:
+#
